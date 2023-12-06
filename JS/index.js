40ec02723c7db292e859e5b3f7186b357b97bd52
FinalActt <evaldaspaulauskas2@gmail.com>
2023-12-06 13:52:06 -0800

    Revert "pabaigtas pb2-pb3"

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+
+// PB-2 EMAIL LAUKAS
+
+function scheduleDemo() {
+    var email = document.getElementById('email').value.trim();
+
+    var lietuviskiSimboliai = /[ąčęėįšųūž]/i.test(email.split('@')[0]);
+
+    var specSimboliaiMasyvas = "!#$%^&*()',?\":{}|<>";
+
+    var emailas = email.split('@');
+    var specSimboliai = [...emailas[0]].some(char => specSimboliaiMasyvas.includes(char));
+
+    var etSimboliai = email.split('@').length === 2;
+    var poAtSimbolioSimboliai = emailas[1].length >= 3;
+
+    var ilgisIkiAtSimbolio = emailas[0].length <= 20;
+
+    if (email === "" || !etSimboliai || !poAtSimbolioSimboliai) {
+        alert('Įveskite el. paštą.');
+    } else if (!ilgisIkiAtSimbolio) {
+        alert('El. paštas turi būti ne ilgesnė kaip 20 simbolių.');
+    } else if (lietuviskiSimboliai || specSimboliai || !etSimboliai) {
+        alert('El. pašto adrese negali būti lietuviškų raidžių, spec. simbolių.');
+    } else {
+        alert('Jūs teisingai įvedėte el. pašto adresą!');
+    }
+}
